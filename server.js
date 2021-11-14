bc8ff5fee6e710af62ac8550ed38e4090546e7d9
BAMBA.AIDARA <caidaraisidk@groupeisi.com>
2021-11-14 01:01:39 +0000

    Master

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const path = require('path');
-const { dirname } = require('path/posix');
 
-const app= express();
 
-app.use(express.static(_dirname + '/dist/touba'));
+const app = express();
+
+app.use(express.static(_dirname + '/dist/touba/'));
 
 app.get('/*',function(req,res) {
   res.sendFile(path.join(_dirname+'/dist/touba/index.html'));
